6b9c9a1187359aa69d960513d76230408a28c2ec
Tobias Berger <tobi.berger13@gmail.com>
2021-02-06 15:18:41 +0100

    Create circles on click at mouse position

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 
 import planck from "planck-js";
-import { CanvasRenderer } from "../lib/renderer";
+import { CanvasRenderer as Renderer } from "../lib/renderer";
 import { Runner } from "../lib/runner";
 
-export class Scene extends React.Component<Record<string, never>> {
+type Props = Record<string, never>;
+
+export class Scene extends React.Component<Props> {
   canvas: React.RefObject<HTMLCanvasElement>;
   toolbar: React.RefObject<HTMLDivElement>;
   world: planck.World;
 
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
     this.world = new planck.World();
+    this.world.setGravity(new planck.Vec2(0, 10));
+
     const groundBody = this.world.createBody({
-      position: new planck.Vec2(0, -10),
+      position: new planck.Vec2(0, 50),
+      angle: 0.02 * Math.PI,
     });
-    const groundBox = new planck.Box(50, 10);
+    const groundBox = new planck.Box(100, 10);
     groundBody.createFixture(groundBox);
 
     const body = this.world.createBody({
@@ -23,11 +28,10 @@ export class Scene extends React.Component<Record<string, never>> {
       position: new planck.Vec2(0, 4),
     });
 
-    const dynamicBox = new planck.Box(1, 1);
+    const dynamicBox = new planck.Circle(planck.Vec2(), 4);
     body.createFixture({
       shape: dynamicBox,
-      density: 1,
-      friction: 0.3,
+      density: 100,
     });
 
     this.canvas = React.createRef();
@@ -37,18 +41,40 @@ export class Scene extends React.Component<Record<string, never>> {
   componentDidMount() {
     const context = this.canvas.current.getContext("2d");
 
-    const renderer = new CanvasRenderer(this.world, context);
+    const renderer = new Renderer(this.world, context, { scale: 1 });
     const runner = new Runner(this.world, { fps: 30 });
 
-    const handleResize = () => {
-      this.canvas.current.width = window.innerWidth;
-      this.canvas.current.height = window.innerHeight - 50;
-    };
+    window.addEventListener("resize", () => this.handleResize());
+    this.handleResize();
 
-    window.addEventListener("resize", handleResize);
-    handleResize();
+    this.canvas.current.addEventListener("click", (event: MouseEvent) => {
+      this.world
+        .createBody({
+          type: "dynamic",
+          position: this.getCursorPositionInCanvas(event),
+        })
+        .createFixture({
+          shape: new planck.Circle(planck.Vec2(), 3),
+          density: 100,
+        });
+    });
 
+    runner.options.speed = 5;
     runner.start(() => renderer.renderWorld());
+  }
+  getCursorPositionInCanvas(event: MouseEvent) {
+    const rect = this.canvas.current.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    return planck.Vec2(x, y);
+  }
+
+  handleResize() {
+    if (!(this.canvas?.current ?? false)) {
+      return console.debug("canvas is not defined. This shouldn't happen.");
+    }
+    this.canvas.current.width = window.innerWidth;
+    this.canvas.current.height = window.innerHeight - 50;
   }
 
   render() {
